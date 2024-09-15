b0cafda64f4e96d9a3dcca86729fd3d12aec1b2b
zoe ng <zoeng6.project@gmail.com>
2024-09-15 20:55:28 +0000

    update functional-test plan

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -13,21 +13,38 @@ const server = require('../server');
 
 chai.use(chaiHttp);
 
+let bookID;
 suite('Functional Tests', function() {
 
   /*
   * ----[EXAMPLE TEST]----
   * Each test should completely test the response of the API end-point including response status code!
   */
-  test('#example Test GET /api/books', function(done){
-     chai.request(server)
-      .get('/api/books')
-      .end(function(err, res){
+  test("#example Test GET /api/books", function (done) {
+    chai
+      .request(server)
+      .get("/api/books")
+      .end(function (err, res) {
         assert.equal(res.status, 200);
-        assert.isArray(res.body, 'response should be an array');
-        assert.property(res.body[0], 'commentcount', 'Books in array should contain commentcount');
-        assert.property(res.body[0], 'title', 'Books in array should contain title');
-        assert.property(res.body[0], '_id', 'Books in array should contain _id');
+        assert.isArray(res.body, "response should be an array");
+        if (!res.body[0]) {
+          done();
+        }
+        assert.property(
+          res.body[0],
+          "commentcount",
+          "Books in array should contain commentcount"
+        );
+        assert.property(
+          res.body[0],
+          "title",
+          "Books in array should contain title"
+        );
+        assert.property(
+          res.body[0],
+          "_id",
+          "Books in array should contain _id"
+        );
         done();
       });
   });
@@ -41,11 +58,28 @@ suite('Functional Tests', function() {
     suite('POST /api/books with title => create book object/expect book object', function() {
       
       test('Test POST /api/books with title', function(done) {
-        //done();
+        chai
+        .request(server)
+        .post("/api/books")
+        .send({ title: "test-title" })
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          bookID = res.body._id;
+          assert.equal(res.body.title, "test-title");
+          done();
+        });
       });
       
       test('Test POST /api/books with no title given', function(done) {
-        //done();
+        chai
+            .request(server)
+            .post("/api/books")
+            .send({})
+            .end(function (err, res) {
+              assert.equal(res.status, 200);
+              assert.equal(res.text, "missing required field tittle");
+              done();
+        });
       });
       
     });
@@ -54,7 +88,14 @@ suite('Functional Tests', function() {
     suite('GET /api/books => array of books', function(){
       
       test('Test GET /api/books',  function(done){
-        //done();
+        chai
+        .request(server)
+        .get("/api/books")
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.isArray(res.body, "it is an array");
+          done();
+        });
       });      
       
     });
@@ -63,28 +104,65 @@ suite('Functional Tests', function() {
     suite('GET /api/books/[id] => book object with [id]', function(){
       
       test('Test GET /api/books/[id] with id not in db',  function(done){
-        //done();
+        chai
+        .request(server)
+        .get("/api/books/invalidID")
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.text, "no book exists");
+          done();
+        });
       });
       
       test('Test GET /api/books/[id] with valid id in db',  function(done){
-        //done();
+        chai
+        .request(server)
+        .get("/api/books/" + bookID)
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.body.title, "test-title");
+          done();
+        });
       });
-      
     });
 
 
     suite('POST /api/books/[id] => add comment/expect book object with id', function(){
       
       test('Test POST /api/books/[id] with comment', function(done){
-        //done();
+        chai
+        .request(server)
+        .post("/api/books/" + bookID)
+        .send({ comment: "test-comment" })
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.body.comments[0], "test-comment");
+          done();
+        });
       });
 
       test('Test POST /api/books/[id] without comment field', function(done){
-        //done();
+        chai
+        .request(server)
+        .post("/api/books/" + bookID)
+        .send({})
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.text, "missing required field comment");
+          done();
+        });
       });
 
       test('Test POST /api/books/[id] with comment, id not in db', function(done){
-        //done();
+        chai
+        .request(server)
+        .post("/api/books/" + "invalidID")
+        .send({ comment: "test-comment" })
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.text, "no book exists");
+          done();
+        });
       });
       
     });
@@ -92,13 +170,26 @@ suite('Functional Tests', function() {
     suite('DELETE /api/books/[id] => delete book object id', function() {
 
       test('Test DELETE /api/books/[id] with valid id in db', function(done){
-        //done();
+        chai
+        .request(server)
+        .delete("/api/books/" + bookID)
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.text, "delete successful");
+          done();
+        });
       });
 
       test('Test DELETE /api/books/[id] with  id not in db', function(done){
-        //done();
+        chai
+        .request(server)
+        .delete("/api/books/" + "invalidID")
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.text, "no book exists");
+          done();
+        });
       });
-
     });
 
   });
